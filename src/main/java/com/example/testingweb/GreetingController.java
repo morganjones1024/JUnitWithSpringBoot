4e6eefe80e95edf95bda4c5e943918d4953d1886
morganjones <morganjones@Morgans-Air.attlocal.net>
2022-06-03 15:04:59 -0400

    homework

---
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.testingweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class GreetingController {
+
+	private final GreetingService service;
+
+	public GreetingController(GreetingService service) {
+		this.service = service;
+	}
+
+	@RequestMapping("/greeting")
+	public @ResponseBody String greeting() {
+		return service.greet();
+	}
+	
+	
+
+}
